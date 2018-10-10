e25592f3d2650cd087948661d8241138bed1b9e5
github username <github email address>
2018-10-10 14:12:24 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+function getFirstSelector(selector){
+    return document.querySelector(selector)
+}
 
+
+function nestedTarget(){
+    return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n){
+    const list = document.querySelectorAll('.ranked-list')
+    
+    for (let i = 0; i < list.length; i++) {
+        list[i].innerHTML = parseInt(list[i].innerHTML + n).toString();
+      }
+    }
+
+
+
+    function deepestChild() {
+        let node = document.getElementById('grand-node')
+        let nextNode = node.children[0]
+      
+        while (nextNode) {
+          node = nextNode
+          nextNode = node.children[0]
+        }
+      
+        return node
+      }
